b2217a9574f545e3a61f303e5c9ba4ffd86335a9
foysal-mahmud <foysal.bu.cse28@gmail.com>
2020-12-12 16:38:58 +0600

    Create User model in App_login/models.py

--- App_Login/models.py
+++ App_Login/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+# To create a Custom User Model and Admin panel
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy
+
 # Create your models here.
+class MyUserManager(BaseUserManager):
+    """ A Custom Manager to deal with emails as unique identifier """
+    def _create_user(self, email, password, **extra_fields):         # username optional thakbe.. email and password diye e kaj hobe.
+        """ Creates and saves a user with email and password """
+
+        if not email:
+            raise ValueError("The Email must be Required!!!")
+
+        email = self.normalize_email(email)   # exactly jemon ase temon likte hobe
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+    
