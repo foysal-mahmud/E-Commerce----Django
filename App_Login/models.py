7bd92da1d7181f53fa7694c10c68122200b9b40e
foysal-mahmud <foysal.bu.cse28@gmail.com>
2020-12-12 17:03:49 +0600

    Create User class and Mapped by OneToOne relationship with Profile class in App_Login/models.py

--- App_Login/models.py
+++ App_Login/models.py
@@ -33,3 +33,55 @@ class MyUserManager(BaseUserManager):
             raise ValueError('SuperUser must have is_superuser=True')
 
         return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, null=False)
+    is_staff = models.BooleanField(
+        ugettext_lazy('staff status'),
+        default=False,
+        help_text = ugettext_lazy('Designates whether the user can log in this site')
+    )
+
+    is_active = models.BooleanField{
+        ugettext_lazy('active'),
+        default=False,
+        help_text = ugettext_lazy('Designates whether the user should be treated as active. Unselect this instead of deleting accounts')
+
+    }
+
+    USERNAME_FIELD = 'email'
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    username = models.CharField(max_length=264, blank=True)
+    full_name = models.CharField(max_length=264, blank=True)
+    address_1 = models.TextField(max_length=300, blank=True)
+    city = models.CharField(max_length=40, blank=True)
+    zipcode = models.CharField(max_length=10, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user + "'s Profile"
+
+    def is_fully_filled(self):
+        fields_names = [f.name for f in self._meta.get_fields()]
+
+        for field_name in fields_names:
+            value = getattr(self, field_name)
+            if value is None or value='':
+                return False
+        return True
