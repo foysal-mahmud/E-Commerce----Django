b7c40b50668b6cd6442d173c5e37ec6886cb58b5
foysal-mahmud <foysal.bu.cse28@gmail.com>
2020-12-13 23:46:28 +0600

    Create Sign Up forms for user

--- App_Login/urls.py
+++ App_Login/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from App_Login import views
 
 app_name = "App_Login"
 
 urlpatterns = [
-
+    path('account/signup/', views.sign_up, name='signup')
 ]
--- App_Login/views.py
+++ App_Login/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+# Authentication Form
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+
+# App_login Models & Form
+from App_Login.models import Profile
+from App_Login.forms import ProfileForm, SignUpForm
 
 # Create your views here.
+
+def sign_up(request):
+    form = SignUpForm()
+    if request.method ='POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse())
+
+    return render(request, 'App_Login/sign_up.html', context={'form':form})
